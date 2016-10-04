a2fa6a7a45823dba00d5e85241e21338fede25a3
Jonas Rudlang <jonas.rudlang@gmail.com>
2016-10-04 19:38:30 +0200

    Change to responsive theme

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,25 +1,21 @@
-<?php
-
-if (app()->environment() != 'testing') {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST');
-    header("Access-Control-Allow-Headers: X-Requested-With");
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
+    <title>Swagger explorer</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!--style type="text/css"></style-->
+
     <title>{{config('l5-swagger.api.title')}}</title>
-    <link rel="icon" type="image/png" href="{{config('l5-swagger.paths.assets_public')}}/images/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="{{config('l5-swagger.paths.assets_public')}}/images/favicon-16x16.png" sizes="16x16" />
-    <link href='{{config('l5-swagger.paths.assets_public')}}/css/typography.css' media='screen' rel='stylesheet' type='text/css'/>
-    <link href='{{config('l5-swagger.paths.assets_public')}}/css/reset.css' media='screen' rel='stylesheet' type='text/css'/>
-    <link href='{{config('l5-swagger.paths.assets_public')}}/css/screen.css' media='screen' rel='stylesheet' type='text/css'/>
-    <link href='{{config('l5-swagger.paths.assets_public')}}/css/reset.css' media='print' rel='stylesheet' type='text/css'/>
-    <link href='{{config('l5-swagger.paths.assets_public')}}/css/print.css' media='print' rel='stylesheet' type='text/css'/>
+
+    <link href="{{config('l5-swagger.paths.assets_public')}}/css/index.css" rel="stylesheet"/>
+    <link href="{{config('l5-swagger.paths.assets_public')}}/css/standalone.css" rel='stylesheet'/>
+    <link href="{{config('l5-swagger.paths.assets_public')}}/css/api-explorer.css" rel='stylesheet' type='text/css'/>
+    <link href="{{config('l5-swagger.paths.assets_public')}}/css/screen.css" media='screen' rel='stylesheet' type='text/css'/>
+
     <script src='{{config('l5-swagger.paths.assets_public')}}/lib/jquery-1.8.0.min.js' type='text/javascript'></script>
     <script src='{{config('l5-swagger.paths.assets_public')}}/lib/jquery.slideto.min.js' type='text/javascript'></script>
     <script src='{{config('l5-swagger.paths.assets_public')}}/lib/jquery.wiggle.min.js' type='text/javascript'></script>
@@ -29,14 +25,22 @@ if (app()->environment() != 'testing') {
     <script src='{{config('l5-swagger.paths.assets_public')}}/lib/backbone-min.js' type='text/javascript'></script>
     <script src='{{config('l5-swagger.paths.assets_public')}}/swagger-ui.js' type='text/javascript'></script>
     <script src='{{config('l5-swagger.paths.assets_public')}}/lib/highlight.7.3.pack.js' type='text/javascript'></script>
-    <script src='{{config('l5-swagger.paths.assets_public')}}/lib/jsoneditor.min.js' type='text/javascript'></script>
+    <script src='{{config('l5-swagger.paths.assets_public')}}/lib/jsoneditor.js' type='text/javascript'></script>
     <script src='{{config('l5-swagger.paths.assets_public')}}/lib/marked.js' type='text/javascript'></script>
     <script src='{{config('l5-swagger.paths.assets_public')}}/lib/swagger-oauth.js' type='text/javascript'></script>
+    <script src='{{config('l5-swagger.paths.assets_public')}}/lib/bootstrap.min.js' type='text/javascript'></script>
 
-    <!-- Some basic translations -->
-    <!-- <script src='lang/translator.js' type='text/javascript'></script> -->
-    <!-- <script src='lang/ru.js' type='text/javascript'></script> -->
-    <!-- <script src='lang/en.js' type='text/javascript'></script> -->
+    <script type="text/javascript">
+        jQuery.browser = jQuery.browser || {};
+        (function () {
+            jQuery.browser.msie = jQuery.browser.msie || false;
+            jQuery.browser.version = jQuery.browser.version || 0;
+            if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
+                jQuery.browser.msie = true;
+                jQuery.browser.version = RegExp.$1;
+            }
+        })();
+    </script>
 
     <script type="text/javascript">
         $(function () {
@@ -47,97 +51,208 @@ if (app()->environment() != 'testing') {
                 url = "{!! $urlToDocs !!}";
             }
 
-            // Pre load translate...
-            if(window.SwaggerTranslator) {
-                window.SwaggerTranslator.translate();
-            }
             window.swaggerUi = new SwaggerUi({
                 url: url,
                 dom_id: "swagger-ui-container",
-                @if(array_key_exists('validatorUrl', get_defined_vars()))
-                // This differentiates between a null value and an undefined variable
-                validatorUrl: {!! isset($validatorUrl) ? '"' . $validatorUrl . '"' : 'null' !!},
-                @endif
                 supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
-                onComplete: function(swaggerApi, swaggerUi){
+                onComplete: function (swaggerApi, swaggerUi) {
                     @if(isset($requestHeaders))
                     @foreach($requestHeaders as $requestKey => $requestValue)
                     window.swaggerUi.api.clientAuthorizations.add("{{$requestKey}}", new SwaggerClient.ApiKeyAuthorization("{{$requestKey}}", "{{$requestValue}}", "header"));
                     @endforeach
-                            @endif
+                    @endif
 
-                    if(typeof initOAuth == "function") {
+                    if (typeof initOAuth == "function") {
+
                         initOAuth({
-                            clientId: "your-client-id",
-                            clientSecret: "your-client-secret-if-required",
-                            realm: "your-realms",
-                            appName: "your-app-name",
-                            scopeSeparator: ",",
-                            additionalQueryStringParams: {}
+                            clientId: "ffe7748a-3a3f-4860-a02a-42ab08e4fde2",
+                            realm: "realm",
+                            appName: "Swagger"
                         });
+
                     }
 
-                    if(window.SwaggerTranslator) {
-                        window.SwaggerTranslator.translate();
-                    }
-
-                    $('pre code').each(function(i, e) {
+                    $('pre code').each(function (i, e) {
                         hljs.highlightBlock(e)
                     });
-                },
 
-                onFailure: function(data) {
-                    console.log("Unable to Load SwaggerUI");
+                    if (swaggerUi.options.url) {
+                        $('#input_baseUrl').val(swaggerUi.options.url);
+                    }
+                    if (swaggerUi.options.apiKey) {
+                        $('#input_apiKey').val(swaggerUi.options.apiKey);
+                    }
+
+                    $("[data-toggle='tooltip']").tooltip();
+
+                    addApiKeyAuthorization();
                 },
-                docExpansion: {!! isset($docExpansion) ? '"' . $docExpansion . '"' : '"none"' !!},
-                jsonEditor: false,
-                apisSorter: "alpha",
-                defaultModelRendering: 'schema',
-                showRequestHeaders: false
+                onFailure: function (data) {
+                    log("Unable to Load SwaggerUI");
+                },
+                docExpansion: "none",
+                sorter: "alpha"
             });
 
-            function addApiKeyAuthorization(){
-                var key = $('#input_apiKey')[0].value;
-
-                if ("{{$apiKeyInject}}" === "query") {
-                    key = encodeURIComponent(key);
-                }
-
-                if(key && key.trim() != "") {
-                    var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization("{{$apiKeyVar}}", key, "{{$apiKeyInject}}");
-                    window.swaggerUi.api.clientAuthorizations.add("{{$securityDefinition}}", apiKeyAuth);
+            function addApiKeyAuthorization() {
+                var key = encodeURIComponent($('#input_apiKey')[0].value);
+                if (key && key.trim() != "") {
+                    var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization("Authorization", "Bearer " + key, "header");
+                    window.swaggerUi.api.clientAuthorizations.add("key", apiKeyAuth);
+                    log("added key " + key);
                 }
             }
 
-            $('#input_apiKey').change(function() {
-                addApiKeyAuthorization();
-            });
+            function addApiKeyAuthorization() {
+                var key = encodeURIComponent($('#input_apiKey')[0].value);
+                if (key && key.trim() != "") {
+                    var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization("Authorization", "Bearer " + key, "header");
+                    @if(isset($requestHeaders))
+                    @foreach($requestHeaders as $requestKey => $requestValue)
+                    window.swaggerUi.api.clientAuthorizations.add("{{$requestKey}}", new SwaggerClient.ApiKeyAuthorization("{{$requestKey}}", "{{$requestValue}}", "header"));
+                    @endforeach
+                    @endif
+                    log("added key " + key);
+                }
+            }
+
+            $('#input_apiKey').change(addApiKeyAuthorization);
+            // if you have an apiKey you would like to pre-populate on the page for demonstration purposes...
+            /*
+             var apiKey = "myApiKeyXXXX123456789";
+             $('#input_apiKey').val(apiKey);
+             */
 
             window.swaggerUi.load();
 
-            // if you have an apiKey you would like to pre-populate on the page for demonstration purposes
-            // just put it in the .env file, API_AUTH_TOKEN variable
-            @if($apiKey)
-            $('#input_apiKey').val("{{$apiKey}}");
-            addApiKeyAuthorization();
-            @endif
+            function log() {
+                if ('console' in window) {
+                    console.log.apply(console, arguments);
+                }
+            }
         });
     </script>
+
+    <script type="text/javascript">
+
+        $(function () {
+
+            $(window).scroll(function () {
+                var sticky = $(".sticky-nav");
+
+                i(sticky);
+                r(sticky);
+
+                function n() {
+                    return window.matchMedia("(min-width: 992px)").matches
+                }
+
+                function e() {
+                    n() ? sticky.parents(".sticky-nav-placeholder").removeAttr("style") : sticky.parents(".sticky-nav-placeholder").css("min-height", sticky.outerHeight())
+                }
+
+                function i(n) {
+                    n.hasClass("fixed") || (navOffset = n.offset().top);
+                    e();
+                    $(window).scrollTop() > navOffset ? $(".modal.in").length || n.addClass("fixed") : n.removeClass("fixed")
+                }
+
+                function r(e) {
+                    function i() {
+                        var i = $(window).scrollTop(), r = e.parents(".sticky-nav");
+                        return r.hasClass("fixed") && !n() && (i = i + r.outerHeight() + 40), i
+                    }
+
+                    function r(e) {
+                        var t = o.next("[data-endpoint]"), n = o.prev("[data-endpoint]");
+                        return "next" === e ? t.length ? t : o.parent().next().find("[data-endpoint]").first() : "prev" === e ? n.length ? n : o.parent().prev().find("[data-endpoint]").last() : []
+                    }
+
+                    var nav = e.find("[data-navigator]");
+                    if (nav.find("[data-endpoint][data-selected]").length) {
+                        var o = nav.find("[data-endpoint][data-selected]"),
+                            a = $("#" + o.attr("data-endpoint")),
+                            s = a.offset().top,
+                            l = (s + a.outerHeight(), r("next")),
+                            u = r("prev");
+                        if (l.length) {
+                            {
+                                var d = $("#" + l.attr("data-endpoint")), f = d.offset().top;
+                                f + d.outerHeight()
+                            }
+                            i() >= f && c(l)
+                        }
+                        if (u.length) {
+                            var p = $("#" + u.attr("data-endpoint")),
+                            g = u.offset().top;
+                            v = (g + p.outerHeight(), 100);
+                            i() < s - v && c(u)
+                        }
+                    }
+                }
+
+                function s() {
+                    var e = $(".sticky-nav [data-navigator]"),
+                        n = e.find("[data-endpoint]").first();
+                    n.attr("data-selected", "");
+                    u.find("[data-selected-value]").html(n.text())
+                }
+
+                function c(e) {
+                    {
+                        var n = $(".sticky-nav [data-navigator]");
+                        $("#" + e.attr("data-endpoint"))
+                    }
+                    n.find("[data-resource]").removeClass("active");
+                    n.find("[data-selected]").removeAttr("data-selected");
+                    e.closest("[data-resource]").addClass("active");
+                    e.attr("data-selected", "");
+                    sticky.find("[data-selected-value]").html(e.text())
+                }
+            });
+
+        });
+    </script>
+
+    <script type="text/javascript">
+        $(function () {
+            $("[data-toggle='tooltip']").tooltip();
+        });
+    </script>
+
 </head>
 
-<body class="swagger-section">
-<div id='header'>
-    <div class="swagger-ui-wrap">
-        <a id="logo" href="http://swagger.io">swagger</a>
-        <form id='api_selector'>
-            <div class='input'><input placeholder="http://example.com/api" id="input_baseUrl" name="baseUrl" type="text"/></div>
-            <div class='input'><input placeholder="api_key" id="input_apiKey" name="apiKey" type="text"/></div>
-            <div class='input'><a id="explore" href="#" data-sw-translate>Explore</a></div>
-        </form>
-    </div>
-</div>
+<body class="page-docs" style="zoom: 1;">
+<header class="site-header">
+    <nav role="navigation" class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" data-toggle="collapse" data-target="#navbar-collapse" class="navbar-toggle"><span
+                        class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span
+                        class="icon-bar"></span><span class="icon-bar"></span></button>
+                <h1 class="navbar-brand"><a href="http://swagger.io"><span>swagger explorer</span></a></h1>
+            </div>
+            <div id="navbar-collapse" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-left">
+                    <li class="li-why"><a href="http://swagger.io" style="font-size: 25px; padding-left: 0px">Swagger
+                        explorer</a></li>
 
-<div id="message-bar" class="swagger-ui-wrap" data-sw-translate>&nbsp;</div>
-<div id="swagger-ui-container" class="swagger-ui-wrap"></div>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<section class="content">
+    <div id="api2-explorer">
+        <div class="swagger-section page-docs" style="zoom: 1">
+            <div class="main-section">
+                <div id="swagger-ui-container" class="swagger-ui-wrap">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 </body>
 </html>
+
